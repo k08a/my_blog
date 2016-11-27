3dc6f643f20217953e76829ca2105010c040a9ee
k08a <etnallirb.a@gmail.com>
2016-11-27 08:55:20 +0000

    new実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,20 @@
 class PostsController < ApplicationController
   def index
+    
   end
 
   def show
   end
 
   def new
+    @post = Post.new
+    
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to root
   end
 
   def edit
@@ -19,4 +25,10 @@ class PostsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def post_params
+      params.require(:posts).permit(:title, :body, :category)
+    end
+
 end
