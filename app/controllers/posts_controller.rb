9a1005046d90c665c836d93641a14f673b0ea9b1
k08a <etnallirb.a@gmail.com>
2016-12-02 12:29:03 +0000

    index.html.erb修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy]
   def index
-    @posts = Post.all
-    @new_posts = Post.all
+    @posts = Post.order(created_at: :desc)
+    @new_posts = Post.order(created_at: :desc).limit(5)
     
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -21,17 +21,14 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to "/posts/#{@post.id}"
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to "/posts/"
     
